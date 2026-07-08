b34be5444a8db14b7ccfbe54adae9633f0aad989
mehmetmertasma <mertasma7580@gmail.com>
2026-07-07 21:00:43 -0500

    Add localization public API

---
+++ TeamSync/src/localization/index.ts
@@ -0,0 +1,4 @@
+export { LanguageProvider, useLanguage } from "@/localization/LanguageProvider";
+export { useTranslation } from "@/localization/useTranslation";
+export { defaultLanguage, supportedLanguages, translations } from "@/localization/translations";
+export type { Language, SupportedLanguage, TranslationDictionary } from "@/localization/types";

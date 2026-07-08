f6f300b1bbb1a9cf8f4c6686422fd627bc98b761
mehmetmertasma <mertasma7580@gmail.com>
2026-07-07 21:19:14 -0500

    Fix legacy i18n import order

--- TeamSync/src/constants/i18n.ts
+++ TeamSync/src/constants/i18n.ts
@@ -1,7 +1,7 @@
+import { defaultLanguage, translations } from "@/localization";
+
 export { defaultLanguage, supportedLanguages, translations } from "@/localization";
 export type { Language, TranslationDictionary as Translations } from "@/localization";
-
-import { defaultLanguage, translations } from "@/localization";
 
 export const t = translations[defaultLanguage];
 export type TranslationKeys = keyof typeof t;

93ac4abe805da98feabfd10e03bc5dcc4eff4c31
mehmetmertasma <mertasma7580@gmail.com>
2026-07-16 22:34:30 -0500

    Add Firestore MaviTeam data service

---
+++ TeamSync/src/services/firestoreMaviTeamDataService.ts
@@ -0,0 +1,17 @@
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  limit as firestoreLimit,
+  query,
+  serverTimestamp,
+  setDoc,
+  where,
+} from "firebase/firestore";
+import type { User } from "firebase/auth";
+import type { QueryDocumentSnapshot } from "firebase/firestore";
+
+import { requireFirebaseServices } from "@/lib/firebase";
+import { firestoreTeamSyncService } from "@/services/firestoreTeamSync

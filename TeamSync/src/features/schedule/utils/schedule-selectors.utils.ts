775fddcc4aafb5f772027dac0de7ae8f88412efd
mehmetmertasma <mertasma7580@gmail.com>
2026-07-20 11:56:14 -0500

    Extract schedule selectors and event grouping

---
+++ TeamSync/src/features/schedule/utils/schedule-selectors.utils.ts
@@ -0,0 +1,36 @@
+import type { ScheduleEvent, TeamSyncAppData } from "@/types/teamSync";
+
+import { getDateKeyFromValue, isEventInMonth } from "./schedule-date.utils";
+
+export function sortScheduleEvents(events: ScheduleEvent[]) {
+  return [...events].sort((firstEvent, secondEvent) => {
+    return new Date(firstEvent.startsAt).getTime() - new Date(secondEvent.startsAt).getTime();
+  });
+}
+
+export function getEventsForMonth(events: ScheduleEvent[], visibleMonth: Date) {
+  return sortScheduleEvents(events).filter((event) => isEventInMonth(event, visibleMonth));
+}
+
+export function groupScheduleEventsByDate(events: ScheduleEvent[]) {
+  return events.reduce<Record<string, ScheduleEvent[]>>((groups, event) => {
+    const dateKey = getDateKeyFromValue(event.startsAt);
+
+    if (dateKey.length === 0) {
+      return groups;
+    }
+
+    return {
+      ...groups,
+      [dateKey]: [...(groups[dateKey] ?? []), event],
+    };
+  }, {});
+}
+
+export function getScheduleTeamLabel(event: ScheduleEvent, appData: TeamSyncAppData) {
+  if (event.teamId === undefined) {
+    return "Tüm Kulüp";
+  }
+
+  return appData.teams.find((team) => team.id === event.teamId)?.name ?? "Takım bulunamadı";
+}
